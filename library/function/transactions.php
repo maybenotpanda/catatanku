2682e40275aab166b2bcb05449c2765f1dd0215f
Duck X Mango <ctrl.fruitman90@gmail.com>
2023-09-10 13:01:16 +0700

    updated layout transactions

--- library/function/transactions.php
+++ library/function/transactions.php
@@ -3,6 +3,6 @@
 function getAllTransactions()
 {
     global $call;
-    $query = mysqli_query($call, "SELECT * FROM transactions");
+    $query = mysqli_query($call, "SELECT * FROM transactions ORDER BY created_at desc");
     return $query;
 }
