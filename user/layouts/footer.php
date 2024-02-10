ae1eef77daffd6f3bed5720c87792d1e4b659b1f
mwputri <ctrl.fruitman90@gmail.com>
2024-02-10 16:25:44 +0700

    updated: version

--- user/layouts/footer.php
+++ user/layouts/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-        <b>Version</b> 2.1.1
+        <b>Version</b> 2.2.2
     </div>
     <strong>Created By &copy; 2021 <a href="demiadektu.com">MyMango.ID</a>.</strong>
 </footer>
