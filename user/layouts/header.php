efbbbfe6b3f82754683484fe32b606cc4f90f9f9
mwputri <ctrl.fruitman90@gmail.com>
2024-02-10 16:31:38 +0700

    updated: header

--- user/layouts/header.php
+++ user/layouts/header.php
@@ -33,7 +33,7 @@
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="../index" class="nav-link">Home</a>
+                    <a href="index" class="nav-link">Home</a>
                 </li>
             </ul>
         </nav>
@@ -42,7 +42,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="../index" class="brand-link">
+            <a href="index" class="brand-link">
                 <img src="<?= assets('img/icon.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">MyMango.ID</span>
             </a>
@@ -74,9 +74,18 @@
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <li class="nav-item">
+                            <a href="index" class="nav-link">
+                                <i class="nav-icon fas fa-home"></i>
+                                <p>
+                                    Dashboard
+                                    <span class="right badge badge-warning">Maintenance</span>
+                                </p>
+                            </a>
+                        </li>
                         <li class="nav-header">NOTE</li>
                         <li class="nav-item">
-                            <a href="page-notes" class="nav-link">
+                            <a href="#" class="nav-link">
                                 <i class="nav-icon fas fa-copy"></i>
                                 <p>
                                     Notes
@@ -85,7 +94,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="#" class="nav-link" disabled>
                                 <i class="nav-icon fas fa-code"></i>
                                 <p>
                                     ToDo
