cfdd501fa7e8d3f574c4951dbe12e3b4da9b46e5
Fadjar Aidil <m.fadjaraidil@gmail.com>
2025-01-23 17:28:15 +0700

    [feat-default]: update default view

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-Hello World!
+<style>
+  a {
+    text-decoration: none;
+  }
+
+  .container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+  }
+</style>
+
+<div class="container">
+  <a href="src/views/pages/accounts">
+    <h4>Akun</h4>
+  </a>
+  <a href="src/views/pages/transaction.php">
+    <h4>Transaksi</h4>
+  </a>
+</div>
