7e0186c7f9cf775060fb4ecdf9254d92456ec7e9
Fadjar Aidil <m.fadjaraidil@gmail.com>
2025-01-24 02:18:18 +0700

    [feat-BE-FE]: add new modal in main layouts

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -5,3 +5,7 @@ include '../../../../mainconfig.php';
 // ------------------------------------------------ BATAS SUCI ------------------------------------------------ //
 
 include_once '../../../modules/StatusModel.php';
+
+include_once '../../../modules/AccountsModel.php';
+include_once '../../../modules/CategoryModal.php';
+include_once '../../../modules/TransactionModal.php';
