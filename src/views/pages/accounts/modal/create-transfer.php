bfc3c5a223acf4a7ab19812a748109bbd8951bb6
Fadjar Aidil <m.fadjaraidil@gmail.com>
2025-02-01 02:27:50 +0700

    [feat-BE]: create new modal for transfer in accounts

---
+++ src/views/pages/accounts/modal/create-transfer.php
@@ -0,0 +1,50 @@
+<div class="modal fade" id="create-transfer">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Add New Transfer</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="../../../controllers/create-transfer.php" method="POST">
+        <div class="modal-body">
+          <div class="form-group">
+            <label>Account From</label>
+            <select class="form-control select2" style="width: 100%;" name="accountFrom">
+              <option selected="selected" value="802b8ad6-d8ea-11ef-8a81-9c2f9dbc986a">Select Options</option>
+              <?php
+              $say = getActivatedAccounts();
+              foreach ($say as $mm) :
+                echo "<option value=" . $mm['uuid'] . ">" . $mm['name'] . "</option>";
+              endforeach;
+              ?>
+            </select>
+          </div>
+          <div class="form-group">
+            <label>Account To</label>
+            <select class="form-control select2" style="width: 100%;" name="accountTo">
+              <option selected="selected" value="802b8ad6-d8ea-11ef-8a81-9c2f9dbc986a">Select Options</option>
+              <?php
+              $say = getActivatedAccounts();
+              foreach ($say as $mm) :
+                echo "<option value=" . $mm['uuid'] . ">" . $mm['name'] . "</option>";
+              endforeach;
+              ?>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="totalTransactions">Balance</label>
+            <input type="text" class="form-control" id="nominalRupiah" placeholder="Total Transactions" name="balance">
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-danger btn-block" data-dismiss="modal">Close</button>
+          <button type="submit" name="request" class="btn btn-dark btn-block">Save.. <i class="fa fa-save"></i></button>
+        </div>
+      </form>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
