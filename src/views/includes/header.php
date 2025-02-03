9db640e3168cb3a71c3c4c6be04ef21e323a263d
Fadjar Aidil <m.fadjaraidil@gmail.com>
2025-02-04 01:01:37 +0700

    [feat-header]: add new menu category

--- src/views/includes/header.php
+++ src/views/includes/header.php
@@ -56,6 +56,14 @@
                 </p>
               </a>
             </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="nav-icon	fas fa-tags"></i>
+                <p>
+                  Category
+                </p>
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
