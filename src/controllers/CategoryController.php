429ec58f4463c6cbe7752d006cf307a759889de3
Panda <maybenotpanda90@gmail.com>
2025-05-12 07:22:30 +0700

    [feat-category]: add new fn update category

--- src/controllers/CategoryController.php
+++ src/controllers/CategoryController.php
@@ -33,6 +33,37 @@ if (isset($_POST['request'])) {
         header('Location:../views/pages/categories?status=500&message=null');
       }
       break;
+    case "updateCategory":
+      // * update Category
+      $uuid         = $_GET['category'];
+      $name         = isset($_POST['name']) ? trim($_POST['name']) : '';
+      $description  = isset($_POST['description']) ? trim($_POST['description']) : '';
+
+      if (empty($name)) {
+        header('Location:../views/pages/categories?status=400&message=Harap Memasukkan Nama Kategori');
+        exit;
+      }
+
+      if (empty($uuid)) {
+        header('Location:../views/pages/categories?status=404&message=Akun Tidak Di temukan');
+        exit;
+      }
+
+      $updateCategory = mysqli_query(
+        $call,
+        "UPDATE category
+        SET
+          name='$name',
+          description='$description',
+          updated_at='$dtme'
+        WHERE uuid='$uuid'"
+      );
+      if ($updateCategory) {
+        header('Location:../views/pages/categories?status=200&message=successfully');
+      } else {
+        header('Location:../views/pages/categories?status=500&message=null');
+      }
+      break;
     default:
       header('Location:../views/pages/categories?status=404&message=not found type');
   }
