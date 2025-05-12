0e4a5bc2292bf001faeefd98db65cc7ac96e108c
Panda <maybenotpanda90@gmail.com>
2025-05-12 07:20:17 +0700

    [feat-category]: add new select data detail

--- src/modules/CategoryModal.php
+++ src/modules/CategoryModal.php
@@ -6,3 +6,10 @@ function getAllCategory()
   $query = mysqli_query($call, "SELECT * FROM category WHERE deleted_at IS null");
   return $query;
 }
+
+function getDetailCategory($uuid)
+{
+  global $call;
+  $query = mysqli_query($call, "SELECT * FROM category  WHERE uuid='$uuid'");
+  return mysqli_fetch_array($query);
+}
