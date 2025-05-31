6df209276a22f67e65327cbe0b23f849e280290f
maybenotpanda <mybenotpanda@gmail.com>
2025-05-31 08:05:27 +0700

    [feat-layout]: update version

--- src/views/includes/footer.php
+++ src/views/includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="float-right d-none d-sm-block">
-    <b>Version</b> 0.1.4
+    <b>Version</b> 0.2.0
   </div>
   <strong>Created By &copy; 2021 <a href="demiadektu.com">MyMango.ID</a>.</strong>
 </footer>
